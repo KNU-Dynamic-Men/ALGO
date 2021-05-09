86703ac723aa87f75e71660fa52767592d59efc0
KJY <rave189@naver.com>
2021-05-09 19:54:14 +0900

    [재연] 2020카카오 인턴십 문제 수정

--- 추가문제/2020  카카오 인턴십/재연/2020 카카오 여름 인턴 3번.java
+++ 추가문제/2020  카카오 인턴십/재연/2020 카카오 여름 인턴 3번.java
@@ -3,18 +3,30 @@ package BOJ;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.HashSet;
 
+/**
+ * 2020 카카오 여름 인턴 문제
+ * 어피치가 보석을 쇼핑하려고 한다.
+ * 어피치는 진열된 모든 보석을 사고 싶어한다.
+ * 이 때, 진열된 모든 종류의 보석을 적어도 1개 이상 포함하는 가장 짧은 구간을 찾아서 구매하려고 한다.
+ * 몇 번 보석부터 몇 번 보석까지 사야되는지 구하는 문제
+ * 가장 짧은 구간이 여러 개라면 시작 진열대 번호가 가장 작은 구간을 구한다.
+ * @author Rave
+ *
+ */
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		Solution s = new Solution();
-		String[] gems = // { "DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD","SAPPHIRE", "DIA" };
-				// {"AA", "AB", "AC", "AA", "AC"};
-				// {"XYZ", "XYZ","XYZ"};
-				{ "ZZZ", "YYY", "NNNN", "YYY", "BBB" };
-
+		String[] gems = { "DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA" };
+		// { "AA", "AB", "AC", "AA", "AC" };
+		// {"XYZ", "XYZ","XYZ"};
+		// { "ZZZ", "YYY", "NNNN", "YYY", "BBB" };
+		// {"A", "B", "B", "C", "A"};
+		// { "A", "A", "B" };
 		int[] result = s.solution(gems);
 		System.out.println(result[0] + " " + result[1]);
 	}
@@ -23,27 +35,46 @@ public class Main {
 class Solution {
 	HashSet<String> totalTypeOfGem = new HashSet<>();
 
+	/**
+	 * 보석의 종류를 구하기 위해 HashSet에 모두 담아 종류를 구한다.
+	 * 이후 투 포인터를 이용하여 한 칸씩 늘려가며 모든 보석의 종류를 살 수 있는지 확인한다.
+	 * start부터 end까지의 보석의 종류가 전체 보석의 종류보다 작다면 end를 늘려 더 많은 보석을 구매해본다.
+	 * 모든 보석을 적어도 한 개씩 구매할 수 있다면 현재 구간이 가장 짧은 구간인지 확인한다.
+	 * 이후 start를 증가시키고, start에 있던 보석을 Hash에서 제거한다.
+	 * @param gems 진열된 보석의 종류
+	 * @return 모든 보석을 적어도 1개씩 사기 위한 가장 짧은 구간
+	 */
 	public int[] solution(String[] gems) {
 		int[] answer = { 1, gems.length };
+		// 전체 보석의 종류를 파악한다.
 		for (String gem : gems)
 			totalTypeOfGem.add(gem);
 		int totalSize = totalTypeOfGem.size();
 		int start = 0;
 		int end = 0;
-		HashSet<String> typeOfGem = new HashSet<>();
+		HashMap<String, Integer> typeOfGem = new HashMap<>();
+		// start가 증가하는 경우 end는 증가하지 않았으므로 put하지 않아야 한다.
+		// 따라서 end가 증가하는 경우에만 put하도록 한다.
+		boolean isIncreaseEnd = true;
 		while (start <= end && end < gems.length) {
-			for (int i = start; i <= end; i++)
-				typeOfGem.add(gems[i]);
-			if (typeOfGem.size() < totalSize)
+			if (isIncreaseEnd)
+				typeOfGem.put(gems[end], typeOfGem.getOrDefault(gems[end], 0) + 1);
+			if (typeOfGem.size() < totalSize) {
 				end++;
-			else if (typeOfGem.size() == totalSize) {
+				isIncreaseEnd = true;
+			} else if (typeOfGem.size() == totalSize) {
 				if (end - start < answer[1] - answer[0]) {
 					answer[0] = start + 1;
 					answer[1] = end + 1;
 				}
+				int remove = typeOfGem.get(gems[start]);
+				if (remove == 1)
+					typeOfGem.remove(gems[start]);
+				else
+					typeOfGem.replace(gems[start], remove - 1);
 				start++;
+				isIncreaseEnd = false;
 			}
-			typeOfGem.clear();
 		}
 		return answer;
 	}
--- 추가문제/2020  카카오 인턴십/재연/2020 카카오 여름 인턴 4번.java
+++ 추가문제/2020  카카오 인턴십/재연/2020 카카오 여름 인턴 4번.java
@@ -6,67 +6,85 @@ import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ * 2020 카카오 여름 인턴 문제
+ * 죠르디가 자동차 경주로를 건설하려고 한다.
+ * 경주로는 (0, 0)에서 (n-1, n-1)칸으로 가는 경주로이다.
+ * 직선 도로는 100의 비용이, 코너는 500의 비용이 든다.
+ * 0은 비어있는 칸, 1은 벽으로 이루어진 board가 주어질 때
+ * 도로 건설의 최소 비용을 구하는 문제
+ * @author Rave
+ *
+ */
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		Solution s = new Solution();
-		 int[][] board = 
-				 //{{0,0,0},{0,0,0},{0,0,0}};
-		 //{{0,0,0,0,0,0,0,1},{0,0,0,0,0,0,0,0},{0,0,0,0,0,1,0,0},{0,0,0,0,1,0,0,0},{0,0,0,1,0,0,0,1},{0,0,1,0,0,0,1,0},{0,1,0,0,0,1,0,0},{1,0,0,0,0,0,0,0}};
-		 //{{0,0,1,0},{0,0,0,0},{0,1,0,1},{1,0,0,0}};
-		 {{0,0,0,0,0,0},{0,1,1,1,1,0},{0,0,1,0,0,0},{1,0,0,1,0,1},{0,1,0,0,0,1},{0,0,0,0,0,0}};
-		 System.out.println(s.solution(board));
+		int[][] board =  { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+				// {{0,0,0,0,0,0,0,1},{0,0,0,0,0,0,0,0},{0,0,0,0,0,1,0,0},{0,0,0,0,1,0,0,0},{0,0,0,1,0,0,0,1},{0,0,1,0,0,0,1,0},{0,1,0,0,0,1,0,0},{1,0,0,0,0,0,0,0}};
+				// {{0,0,1,0},{0,0,0,0},{0,1,0,1},{1,0,0,0}};
+				//{ { 0, 0, 0, 0, 0, 0 }, { 0, 1, 1, 1, 1, 0 }, { 0, 0, 1, 0, 0, 0 }, { 1, 0, 0, 1, 0, 1 },
+						//{ 0, 1, 0, 0, 0, 1 }, { 0, 0, 0, 0, 0, 0 } };
+		System.out.println(s.solution(board));
 	}
 }
 
 class Solution {
-	int[][] visited;
+	int[][] distance;
 	int[] dx = { 0, 1, 0, -1 };
 	int[] dy = { 1, 0, -1, 0 };
 
+	/**
+	 * 자동차는 (0, 0)에서 오른쪽과 아래 방향으로 갈 수 있으므로 2개의 포인트를 추가하고 시작한다.
+	 * 이후 dx, dy의 i번째 인덱스와 자동차의 방향이 같다면 직선도로, 다르다면 코너이다.
+	 * 이를 활용하여 BFS 탐색을 시도한다.
+	 * 코너의 값이 직선도로보다 크므로 탐색을 끝까지 시도해본다.
+	 * @param board 자동차 경주로의 설계 도면
+	 * @return 경주로를 건설하는데 드는 최소 비용
+	 */
 	public int solution(int[][] board) {
+		int answer = Integer.MAX_VALUE;
 		int n = board.length;
-		visited = new int[n][n];
+		distance = new int[n][n];
 		Queue<Point> q = new LinkedList<>();
-		q.add(new Point(0, 0, 0));
-		q.add(new Point(0, 0, 1));
+		q.add(new Point(0, 0, 0, 0));
+		q.add(new Point(0, 0, 1, 0));
 		while (!q.isEmpty()) {
 			Point cur = q.poll();
-			if (cur.x == n - 1 && cur.y == n - 1)
+			if (cur.x == n - 1 && cur.y == n - 1) {
+				answer = Math.min(answer, cur.cost);
 				continue;
+			}
 			for (int i = 0; i < 4; i++) {
 				int nextX = cur.x + dx[i];
 				int nextY = cur.y + dy[i];
 				try {
 					if (board[nextX][nextY] == 1)
 						continue;
-					int nextCost = visited[cur.x][cur.y] + 100;
+					// distance의 값이 아니라 cur의 cost값을 이용해야 한다. <- 왜 안햇지?
+					int nextCost = cur.cost + 100;
 					if (cur.direction != i)
 						nextCost += 500;
-					if (visited[nextX][nextY] == 0 || nextCost <= visited[nextX][nextY]) {
-						visited[nextX][nextY] = nextCost;
-						;
-						q.add(new Point(nextX, nextY, i));
+					if (distance[nextX][nextY] == 0 || nextCost <= distance[nextX][nextY]) {
+						distance[nextX][nextY] = nextCost;
+						q.add(new Point(nextX, nextY, i, nextCost));
 					}
 				} catch (Exception e) {
 				}
 			}
 		}
-		return visited[n - 1][n - 1];
+		return answer;
 	}
 }
 
 class Point {
-	int x, y, direction;
+	int x, y, direction, cost;
 
-	public Point(int _x, int _y, int _direction) {
+	public Point(int _x, int _y, int _direction, int _cost) {
 		this.x = _x;
 		this.y = _y;
 		this.direction = _direction;
-	}
-
-	public String toString() {
-		return x + " " + y + " " + direction;
+		this.cost = _cost;
 	}
 }
